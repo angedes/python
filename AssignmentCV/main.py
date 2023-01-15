a44f3bfb974e6b52e1bd380b50067ffed4426ac8
angedes <114485461+angedes@users.noreply.github.com>
2023-01-15 11:57:48 +0100

    Update main.py

--- AssignmentCV/main.py
+++ AssignmentCV/main.py
@@ -5,38 +5,159 @@ import plotData as pld
 
 from skimage.feature import match_descriptors
 from skimage.measure import ransac
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def run_sfm(K1, K2, img1, img2, X=None, Y=None, Z=None):
+    # formula is P1 = K1@R_t_1; P2 = K2@R_t_2   with our case K1 =K2
+
+    R_t_1 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
+    R_t_2 = np.empty((3, 4))
+    P1 = K1 @ R_t_1
+    P2 = np.empty((3, 4))
+    X = np.array([]) if X is None else X
+    Y = np.array([]) if Y is None else Y
+    Z = np.array([]) if Z is None else Z
+
+    sift = cv.SIFT_create()
+    kp0, desc0 = sift.detectAndCompute(img1, None)
+    kp1, desc1 = sift.detectAndCompute(img2, None)
+
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=100)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(desc0, desc1, k=2)
+
+    good = []
+    pts1 = []
+    pts2 = []
+
+    # Lowe's paper
+
+    for i, (m, n) in enumerate(matches):
+        if m.distance < 0.7 * n.distance:
+            good.append(m)
+            pts1.append(kp0[m.queryIdx].pt)
+            pts2.append(kp1[m.trainIdx].pt)
+
+    pts1 = np.array(pts1)
+    pts2 = np.array(pts2)
+    F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_RANSAC)
+
+    # prendi inlier points
+    pts1 = pts1[mask.ravel() == 1]
+    pts2 = pts2[mask.ravel() == 1]
+
+    # operazione standard
+    # da te K2 == K1
+    E = np.matmul(np.matmul(np.transpose(K2), F), K1)
+    retval, R, t, mask = cv.recoverPose(E, pts1, pts2, K2)
+
+    R_t_2[:3, :3] = np.matmul(R, R_t_1[:3, :3])
+    R_t_2[:3, 3] = R_t_1[:3, 3] + np.matmul(R_t_1[:3, :3], t.ravel())
+
+
+    print("R_t_1 \n" + str(R_t_1))
+    print("R_t_2 \n" + str(R_t_2))
+
+    P2 = np.matmul(K2, R_t_2)
+
+    print("The projection matrix P1 \n" + str(P1))
+    print("The projection matrix P2 \n" + str(P2))
+
+    pts1 = np.transpose(pts1)
+    pts2 = np.transpose(pts2)
+
+    points_3d = cv.triangulatePoints(P1, P2, pts1, pts2)
+    points_3d /= points_3d[3]
+    print('points_3d = ', points_3d)
+
+    X = np.concatenate((X, points_3d[0]))
+    Y = np.concatenate((Y, points_3d[1]))
+    Z = np.concatenate((Z, points_3d[2]))
+
+    # 3D points in X, Y, Z coordinates
+
+    # print("X 3D = ", X)
+    # print("Y 3D = ", Y)
+    # print("Z 3D = ", Z)
+
+    return R_t_1, R_t_2, P1, P2, X, Y, Z, points_3d
 
 
 
-def main():
+def calc_rep_error(P1, P2, K, R_t_1, R_t_2, imagePoint1, imagePoint2):
+
+    imagePoint1, imagePoint2 = imagePoint1[:2, :], imagePoint2[:2, :]
+    R_t_1 = R_t_1.astype(np.float64)
+    #print('imagePoint1.shape', imagePoint1.shape)
+    #print('imagePoint2.shape', imagePoint2.shape)
+    #print('P1.shape', P1.shape)
+    #print('P2.shape', P2.shape)
+
+    # Triangulate
+
+    point3D = cv.triangulatePoints(P1, P2, imagePoint1, imagePoint2).T
+    #print('points 3D =', point3D)
+    point3D = point3D[:, :3] / point3D[:, 3:4]
+    print('points 3D =', point3D)
+
+    # We can filter 3D points based on if the z component it is negative and so we selected only with z > 0
+    point3D = point3D[point3D[:, 2] > 0,:]
+    print('points 3D filtered =', point3D)
+
+    # Reproject back into the two cameras
+    R1, t1 = R_t_1[:, :3], R_t_1[:, 3]
+    R2, t2 = R_t_2[:, :3], R_t_2[:, 3]
+    t1 = np.matmul(R1.T, t1)
+    t2 = np.matmul(R2.T, t2)
+
+    #print('R1.dtype', R1.dtype)
+    #print('R_t_2.shape',  R_t_2.shape)
+
+    #print('R_t_2', R_t_2)
+
+    rvec1, _ = cv.Rodrigues(R1)
+    rvec2, _ = cv.Rodrigues(R2)
+
+    p1, _ = cv.projectPoints(point3D, rvec1, t1, K, distCoeffs=None)
+    p2, _ = cv.projectPoints(point3D, rvec2, t2, K, distCoeffs=None)
+
+    print('p1.shape =', p1.shape)
+    print('p2.shape =', p2.shape)
+
+    # measure difference between original image point and reprojected image point
+
+    reprojection_error1 = np.linalg.norm(imagePoint1 - p1[:,0,:].T, axis = 0)
+    reprojection_error2 = np.linalg.norm(imagePoint2 - p2[:,0,:].T, axis = 0)
+
+    print(reprojection_error1, reprojection_error2)
+
+
+
+if __name__== '__main__':
+
+    # matching between new1 and the new2 images
+
     match_data = np.load("new1_new2_matches.npz")
 
     x1_SPSG = match_data['keypoints0']
     x2_SPSG = match_data['keypoints1']
     x1_SPSG = np.vstack((x1_SPSG.T, np.ones((1, x1_SPSG.shape[0]))))
     x2_SPSG = np.vstack((x2_SPSG.T, np.ones((1, x2_SPSG.shape[0]))))
-    # descs1_SPSG = match_data['descriptors0'].T
-    # descs2_SPSG = match_data['descriptors1'].T
+
 
     print("Keypoints 0 : ", x1_SPSG)
     print("Keypoints 0 shape = : ", x1_SPSG.shape)
     print("Keypoints 1 : ", x2_SPSG)
     print("Keypoints 1 shape = : ", x2_SPSG.shape)
-    # print("Descriptors 0 : ", descs1_SPSG)
-    # print("Descriptors 1 : ", descs2_SPSG)
 
     matches_SG = match_data['matches']
     matches_SG_confidence = match_data['match_confidence']
 
     print("Matches : ", matches_SG)
     print("Matches confidence : ", matches_SG_confidence)
-
-
-
-
-if __name__== '__main__':
-
-    main()
 
     img1 = cv.cvtColor(cv.imread("new1.png"), cv.COLOR_BGR2RGB)
     img2 = cv.cvtColor(cv.imread("new2.png"), cv.COLOR_BGR2RGB)
@@ -52,16 +173,6 @@ if __name__== '__main__':
 
     KC = K @ I_3_4
 
-    match_data = np.load("new1_new2_matches.npz")
-
-    x1_SPSG = match_data['keypoints0']
-    x2_SPSG = match_data['keypoints1']
-    x1_SPSG = np.vstack((x1_SPSG.T, np.ones((1, x1_SPSG.shape[0]))))
-    x2_SPSG = np.vstack((x2_SPSG.T, np.ones((1, x2_SPSG.shape[0]))))
-    #descs1_SPSG = match_data['descriptors0'].T
-    #descs2_SPSG = match_data['descriptors1'].T
-    matches_SG = match_data['matches']
-    matches_SG_confidence = match_data['match_confidence']
 
     len_matches = 0
 
@@ -74,8 +185,8 @@ if __name__== '__main__':
 
     j = 0
 
-    print('matches_SG.shape=', matches_SG.shape)
-    print('len_matches = : ', len_matches)                           # len_matches = 261
+    #print('matches_SG.shape=', matches_SG.shape)
+    #print('len_matches = : ', len_matches)
 
     for i in range(len(matches_SG)):
         if matches_SG[i] != -1:
@@ -91,9 +202,9 @@ if __name__== '__main__':
 
 
     print('x1_matches_SG: ', x1_matches_SG)
-    print('x1_matches.shape = : ', x1_matches_SG.shape)
+    print('x1_matches_SG.shape = : ', x1_matches_SG.shape)
     print('x2_matches_SG: ', x2_matches_SG)
-    print('x2_matches.shape = : ', x2_matches_SG.shape)
+    print('x2_matches_SG.shape = : ', x2_matches_SG.shape)
 
     fig = plt.figure()
     ax = fig.add_subplot(1, 2, 1)
@@ -107,82 +218,12 @@ if __name__== '__main__':
     plt.show()
 
 
-    def run_sfm(K0, K1, img1, img2 , X=None, Y=None, Z=None):
-
-        R_t_0 = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0]])
-        R_t_1 = np.empty((3, 4))
-        P1 = np.matmul(K0, R_t_0)
-        P2 = np.empty((3, 4))
-        X = np.array([]) if X is None else X
-        Y = np.array([]) if Y is None else Y
-        Z = np.array([]) if Z is None else Z
-
-        sift = cv.SIFT_create()
-        kp0, desc0 = sift.detectAndCompute(img1, None)
-        kp1, desc1 = sift.detectAndCompute(img2, None)
-
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=100)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(desc0,desc1,k=2)
-
-        good = []
-        pts1 = []
-        pts2 = []
-
-        # Lowe's paper
-
-        for i, (m, n) in enumerate(matches):
-            if m.distance < 0.7 * n.distance:
-                good.append(m)
-                pts1.append(kp0[m.queryIdx].pt)
-                pts2.append(kp1[m.trainIdx].pt)
-
-        pts1 = np.array(pts1)
-        pts2 = np.array(pts2)
-        F, mask = cv.findFundamentalMat(pts1, pts2, cv.FM_RANSAC)
-
-        # prendi inlier points
-        pts1 = pts1[mask.ravel() == 1]
-        pts2 = pts2[mask.ravel() == 1]
-
-        # operazione standard
-        # da te K1 == K0, ma non mi ricordo se siano corrette come le ho messe
-        E = np.matmul(np.matmul(np.transpose(K1), F), K0)
-        retval, R, t, mask = cv.recoverPose(E, pts1, pts2, K1)
-
-        R_t_1[:3, :3] = np.matmul(R, R_t_0[:3, :3])
-        R_t_1[:3, 3] = R_t_0[:3, 3] + np.matmul(R_t_0[:3, :3], t.ravel())
-
-        # dovrebbe essere un'identita' la R_t_0
-
-        print("R_t_0 \n" + str(R_t_0))
-        print("R_t_1 \n" + str(R_t_1))
-
-        P2 = np.matmul(K1, R_t_1)
-
-        print("The projection matrix P1 \n" + str(P1))
-        print("The projection matrix P2 \n" + str(P2))
-
-        pts1 = np.transpose(pts1)
-        pts2 = np.transpose(pts2)
-
-        points_3d = cv.triangulatePoints(P1, P2, pts1, pts2)
-        points_3d /= points_3d[3]
-
-        X = np.concatenate((X, points_3d[0]))
-        Y = np.concatenate((Y, points_3d[1]))
-        Z = np.concatenate((Z, points_3d[2]))
-
-        print("X 3D = ", X)
-        print("Y 3D = ", Y)
-        print("Z 3D = ", Z)
-
-
     # La funzione mi sputa fuori le matrici di rotazione, di proiezione e i punti triangolarizzati in 3D
 
-    X = run_sfm(K, K, img1 , img2, X=None, Y=None, Z=None )
+    R_t_1, R_t_2, P1, P2, X, Y, Z , points_3d = run_sfm(K, K, img1 , img2, X=None, Y=None, Z=None )
+
+
+    #w = calc_rep_error(P1, P2, K, R_t_1, R_t_2, x1_matches_SG, x2_matches_SG)
 
 
 
@@ -207,76 +248,157 @@ if __name__== '__main__':
 
 
 
-'''
-    F = fRANSAC.fRANSAC_superglue()
-    print('F = ', F)
-    E = pld.getEfromF(F,K,K)
-    print('E = ', E)
 
-    # SVD triangulation
 
-    # But I need projection matrix for the triangularization
-    # Prepare a matrix for storing the triangulated points, shape (4,103)
-    X_w_tri = np.ones(x1_w.shape)
 
-    for p in range(x1_matches_SG.shape[1]):
-        xi = x1[0, p]
-        yi = x1[1, p]
-        eq1 = [xi * P_1[2, 0] - P_1[0, 0], xi * P_1[2, 1] - P_1[0, 1], xi * P_1[2, 2] - P_1[0, 2],
-               xi * P_1[2, 3] - P_1[0, 3]]
-        eq2 = [yi * P_1[2, 0] - P_1[1, 0], yi * P_1[2, 1] - P_1[1, 1], yi * P_1[2, 2] - P_1[1, 2],
-               yi * P_1[2, 3] - P_1[1, 3]]
+    '''
+    # matching between new2 and the old images
 
-        xi = x2[0, p]
-        yi = x2[1, p]
-        eq3 = [xi * P_2[2, 0] - P_2[0, 0], xi * P_2[2, 1] - P_2[0, 1], xi * P_2[2, 2] - P_2[0, 2],
-               xi * P_2[2, 3] - P_2[0, 3]]
-        eq4 = [yi * P_2[2, 0] - P_2[1, 0], yi * P_2[2, 1] - P_2[1, 1], yi * P_2[2, 2] - P_2[1, 2],
-               yi * P_2[2, 3] - P_2[1, 3]]
+    match_data_34 = np.load("new_old_matches.npz")
 
-        A = np.stack([eq1, eq2, eq3, eq4])
+    x3_SPSG = match_data_34['keypoints0']
+    x4_SPSG = match_data_34['keypoints1']
+    x3_SPSG = np.vstack((x3_SPSG.T, np.ones((1, x3_SPSG.shape[0]))))
+    x4_SPSG = np.vstack((x4_SPSG.T, np.ones((1, x4_SPSG.shape[0]))))
+    # descs3_SPSG = match_data['descriptors0'].T
+    # descs4_SPSG = match_data['descriptors1'].T
 
-        u, s, vh = np.linalg.svd(A)
+    print("Keypoints 0 : ", x3_SPSG)
+    print("Keypoints 0 shape = : ", x3_SPSG.shape)
+    print("Keypoints 1 : ", x4_SPSG)
+    print("Keypoints 1 shape = : ", x4_SPSG.shape)
 
-        p_3d = vh[3] / vh[3, 3]
-        X_w_tri[0, p] = p_3d[0]
-        X_w_tri[1, p] = p_3d[1]
-        X_w_tri[2, p] = p_3d[2]
+    matches_SG_34 = match_data_34['matches']
+    matches_SG_confidence_34 = match_data_34['match_confidence']
 
-    # Plot ground truth next to triangulated 3d points
-    fig3D = plt.figure(1)
+    print("Matches_34 : ", matches_SG_34)
+    print("Matches confidence_34 : ", matches_SG_confidence_34)
 
-    # Plot legend
-    ax = plt.axes(projection='3d', adjustable='box')
-    ax.set_title('Ground truth (blue) and triangulated points (red)')
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
-    ax.set_zlabel('Z')
+    img3 = cv.cvtColor(cv.imread("new.png"), cv.COLOR_BGR2RGB)
+    img4 = cv.cvtColor(cv.imread("old.png"), cv.COLOR_BGR2RGB)
+    new3 = cv.resize(img3, dsize=(640, 480))
+    old = cv.resize(img4, dsize=(640, 480))
 
-    # Plot cameras and world frames
-    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
-    drawRefSystem(ax, T_w_c1, '-', 'C1')
-    drawRefSystem(ax, T_w_c2, '-', 'C2')
 
-    # Plot ground truth
-    drawRefSystem(ax, T_w_c1, '-', 'C1')
-    drawRefSystem(ax, T_w_c2, '-', 'C2')
-    ax.scatter(X_w[0, :], X_w[1, :], X_w[2, :], color='b', marker='.')
-    # Plot triangulated points
-    ax.scatter(X_w_tri[0, :], X_w_tri[1, :], X_w_tri[2, :], color='r', marker='.')
+    len_matches_34 = 0
+
+    for i in range(len(matches_SG_34)):
+        if matches_SG_34[i] != -1:
+            len_matches_34 += 1
+
+    x3_matches_SG = np.empty([3, len_matches_34])
+    x4_matches_SG = np.empty([3, len_matches_34])
+
+    j = 0
+
+    print('matches_SG_34.shape=', matches_SG_34.shape)
+    print('len_matches = : ', len_matches)
+
+    for i in range(len(matches_SG_34)):
+        if matches_SG_34[i] != -1:
+            x3_matches_SG[0, j] = x3_SPSG[0, i]
+            x3_matches_SG[1, j] = x3_SPSG[1, i]
+            x3_matches_SG[2, j] = 1
+
+            x4_matches_SG[0, j] = x4_SPSG[0, matches_SG_34[i]]
+            x4_matches_SG[1, j] = x4_SPSG[1, matches_SG_34[i]]
+            x4_matches_SG[2, j] = 1
+
+            j += 1
+
+    print('x3_matches_SG: ', x3_matches_SG)
+    print('x3_matches_SG.shape = : ', x3_matches_SG.shape)
+    print('x4_matches_SG: ', x4_matches_SG)
+    print('x4_matches_SG.shape = : ', x4_matches_SG.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1)
+    plt.imshow(new3)
+    ax.set_title('New picture')
+    pld.plotMarkersImagePoints(x3_matches_SG, color='b', marker='+')
+    ax = fig.add_subplot(1, 2, 2)
+    plt.imshow(old)
+    pld.plotMarkersImagePoints(x4_matches_SG, color='r', marker='+')
+    ax.set_title('Old picture')
     plt.show()
 
-    plt.close(1)
-
-    img1 = cv2.cvtColor(cv2.imread("new1.png"), cv2.COLOR_BGR2RGB)
-    img2 = cv2.cvtColor(cv2.imread("new2.png"), cv2.COLOR_BGR2RGB)
+    '''
 
 
-    # Ora vorrei applicare SfM (in plotData.py) in cui vado a fare svd di E
-    # Structure from motion
 
-    X_tri_c1, T_c1_c1, T_c1_c2 = pld.SfM(E, K, K, x1_matches_SG, x2_matches_SG, plot=True, img1=new1.png, img2=new2.png)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    '''
+   
+
+   
     F = fRANSAC.fRANSAC_superglue()
     E = K.T @ F @ K
     print('E = ', E)
@@ -306,4 +428,34 @@ if __name__== '__main__':
       camera2, però se setti implicitamente il world reference frame al reference camera2, ottieni la trasformazione camera1 to 
       world (cioè extrinsics), e la trasformazione camera2 to world si riduce ad un'identità.
       
-      '''
+      
+      
+      
+      Nella procudera hai bisogno di R_t per calcolarti R e t (ti servono per fare la projection), R_t (sia per 1 e 2, cioe' 
+      R_t_1 e R_t_2) e' gia' calcolata in SfM, quindi li prendi da li e li sbatti in questa nuova funzione, cioe' la signature 
+      della funzione e' : def calc_rep_error(P1, P2, K, R_t_1, R_t_2, imagePoint1, imagePoint2).
+      Devi aver P1 e P2 shape [3,4] e i points sovrebbero essere [2,N] (con N uguali in entrambi i points).
+      Per il resto se hai cose strane, guarda le shape se tornano. Se l'errore e' molto grande, prima prova a printare 
+      R2, t2 in calc_rep_error e in R,t in run_sfm. Dovrebbero essere identiche le R e le t.
+     
+    '''
+
+    '''
+    A = np.eye(3, 4)
+    pts = np.zeros([2, 10])
+    points3D = cv.triangulatePoints(A, A, pts, pts)
+    print(points3D.shape)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    x = points_3d[0]
+    y = points_3d[1]
+    z = points_3d[2]
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.scatter(x, y, z)
+    plt.show()
+    '''
